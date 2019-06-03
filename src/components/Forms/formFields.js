f9abe77426dcf05d32c2faa0c06b08e056987ce7
Didac <didac@compsaonline.com>
2019-06-03 16:44:50 +0200

    temp

---
+++ src/components/Forms/formFields.js
@@ -0,0 +1,152 @@
+import React from 'react';
+
+import Aux from '../../hoc/Auxiliar/Auxiliar';
+import styles from './formfields.module.scss';
+
+const FormFields = (props) => {
+    
+    const renderFields = () => {
+        const formArray = [];
+
+        for(let elementName in props.formData) {
+            formArray.push({
+                id: elementName,
+                settings: props.formData[elementName]
+            })
+        }
+
+        return formArray.map( (item,i) =>{
+            return (
+                <div key={i} className={styles.Form_element}>
+                    {renderTemplates(item)}
+                </div>
+            )
+        })
+    }
+
+    const showLabel = (show,label) => {
+        return show ? 
+            <label>{label}</label>
+        : null
+    }
+
+    const changeHandler = (event,id,blur) => {
+        const newState = props.formData;
+        newState[id].value = event.target.value;
+
+        if(blur){
+            let validData = validate(newState[id])
+            newState[id].valid = validData[0];
+            newState[id].validationMessage = validData[1];
+        }
+        newState[id].touched = blur;
+
+        props.change(newState);
+    }
+
+    const validate = (element) => {
+        let error = [true,''];
+
+        // minLen
+        if(element.validation.minLen){
+            const valid = element.value.length >= element.validation.minLen;
+            const message = `${ !valid ? `Must be greater than ${element.validation.minLen}` : ''}`;
+            error = !valid ? [valid,message] : error;
+        }
+
+        // required
+        if(element.validation.required){
+            const valid = element.value.trim() !== '';
+            const message = `${ !valid ? 'This field is required' : ''}`;
+            error = !valid ? [valid,message] : error;
+        }
+
+        return  error;
+    }
+
+    const showValidation = (data) => {
+        let errorMessage = null;
+
+        if(data.validation && !data.valid){
+            errorMessage = (
+                <div className="label_error">
+                    {data.validationMessage}
+                </div>
+            )
+        }
+        return errorMessage;
+    }
+
+    const renderTemplates = (data) => {
+        let formTemplate = null;
+        let values = data.settings;
+
+        switch(values.element) {
+            case 'input':
+                // auto generated attr. macke sure they mach the patter name.
+                formTemplate = (
+                    <Aux>
+                        {showLabel(values.label,values.labelText)}                        
+                        <input 
+                            {...values.config}                            
+                            value={values.value}
+                            onBlur = {
+                                (event) => changeHandler(event,data.id,true) 
+                            }
+                            onChange = {
+                                (event) => changeHandler(event,data.id,false)
+                            }
+                        />
+                        {showValidation(values)}
+                    </Aux>
+                )
+                break;
+            case 'textarea':
+                formTemplate = (
+                    <Aux>
+                        {showLabel(values.label,values.labelText)}                        
+                        <textarea 
+                            {...values.config}                            
+                            value={values.value}
+                            onChange = {
+                                (event) => changeHandler(event,data.id)
+                            }
+                        ></textarea>
+                    </Aux>
+                )
+            break;
+            case 'select':
+                formTemplate = (
+                    <Aux>
+                        {showLabel(values.label,values.labelText)} 
+                        <select
+                            value={values.value}
+                            name={values.config.name}
+                            onChange = {
+                                (event) => changeHandler(event,data.id)
+                            }
+                        >
+                            { values.config.options.map( (item,i) =>(
+                                <option key={i} value={item.val}>
+                                    {item.text}
+                                </option>
+                            ))}
+                        </select>
+                    </Aux>
+                )
+                break;
+            default:
+                formTemplate = null;
+        }
+
+        return formTemplate;
+    }
+
+    return (
+        <div>
+            {renderFields()}
+        </div>
+    );
+};
+
+export default FormFields;
