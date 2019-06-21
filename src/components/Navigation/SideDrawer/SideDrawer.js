548c82c464b452bc968215b8757c236b207eaa0c
Didac <didac@compsaonline.com>
2019-06-21 17:38:14 +0200

    more styling

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-//import exchangeLogo from '../../../assets/images/logo.png';
+import exchangeLogo from '../../../assets/images/logo.png';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import styles from './SideDrawer.module.scss';
 import Backdrop from '../../UI/Backdrop/Backdrop';
@@ -17,9 +17,10 @@ const sideDrawer = (props) => {
         <Auxiliar>
             <Backdrop show={props.open} clicked={props.closed}/>
             <div className={attachedStyles}>
-                {/* <div className={styles.Logo}>
+                <div className={styles.Logo}>
                     <img src={exchangeLogo} alt="exchange"></img>
-                </div>         */}
+                    <div>Exchange</div>
+                </div>
                 <nav>
                     <NavigationItems />
                 </nav>
