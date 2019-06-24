db2420b0736add46cd49a463bfd3e98b722f9ab6
unknown <l4wl3ss1989@gmail.com>
2019-06-25 01:57:36 +0200

    update

--- src/axios.exchange.js
+++ src/axios.exchange.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { SERVER_NAME } from './configurations/servername';
 
 const instance =  axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: SERVER_NAME
 });
 
 export default instance;
